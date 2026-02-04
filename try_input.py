f9d36de513ba72f3303ac615f99a952b066290f5
suiyumanbu <suiyumanbu@noreply.gitcode.com>
2026-02-04 10:46:18 +0800

    2026.2.4

--- try_input.py
+++ try_input.py
@@ -1,8 +1,8 @@
 import argparse
 def Get_args():
     parser = argparse.ArgumentParser(description='try_input')
-    parser.add_argument('--input1', type=str, help='the first input')
-    parser.add_argument('--input2', type=str, help='the second input')
+    parser.add_argument('--input1', type=int, help='the first input')
+    parser.add_argument('--input2', type=int, help='the second input')
     args = parser.parse_args()
     return args
 
@@ -12,3 +12,4 @@ if __name__ == '__main__':
     print("================================================")
     print(args.input1)
     print(args.input2)
+    print(f"add them together and get:{args.input1+args.input2}")
